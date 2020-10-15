dbf74f747d14e3b646b7de7a4bd46b5000f8f9f5
Alan Wolfe <alan.wolfe@gmail.com>
2020-10-14 19:03:59 -0700

    making editor friendlier for use

--- editor/config.h
+++ editor/config.h
@@ -1,3 +1,13 @@
+#if __has_include("df_serialize_editor_config.h")
+
+#include "df_serialize_editor_config.h"
+
+#elif __has_include("../df_serialize_editor_config.h")
+
+#include "../df_serialize_editor_config.h"
+
+#else
+
 // include your schema(s) and any headers they need for their types
 #include <string>
 #include <array>
@@ -6,3 +16,5 @@
 
 // Set the root document type
 #define RootDocumentType Lifeforms::Root
+
+#endif
